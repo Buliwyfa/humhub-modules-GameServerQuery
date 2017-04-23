3581cd919f9519dbe35e3deedd2dfec4694be9e8
globefreak <globefreak@gmail.com>
2017-04-23 15:41:07 +0200

    minified css

--- Assets.php
+++ Assets.php
@@ -11,7 +11,7 @@ use yii\web\AssetBundle;
 class Assets extends AssetBundle {
 
     public $css = [
-        'game_server_query.css'
+        'game_server_query.min.css'
     ];
 
     public function init() {
